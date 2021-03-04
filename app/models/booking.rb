1498bb6a28d19398df97c41991a72e9fccd2d253
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-03-04 19:20:53 +0300

    added end_date must come after start_date validation to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,13 @@ class Booking < ApplicationRecord
   has_one :review
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validate :end_date_cannot_come_before_start_date
   # validates :total_price, presence: true
   # validates :approval, presence: true
+
+  def end_date_cannot_come_before_start_date
+    if end_date.present? && end_date < start_date
+      errors.add(:end_date, "end date must come after start date")
+    end
+  end
 end
