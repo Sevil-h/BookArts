fd88b4a6f13050a86c062b94ca4dc54063b01ea8
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-03-04 13:30:30 +0300

    set up simple form for review

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :artwork
-  has_many :reviews
+  has_one :review
   validates :start_date, presence: true
   validates :end_date, presence: true
   # validates :total_price, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :artworks, only: [:index, :show, :new, :create, :edit, :destroy] do 
-  resources :bookings, only: [:create]
+    resources :bookings, only: [:create]
   end
   resource :dashboard, only: :show
+  resources :bookings, only: [] do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: :index
 end
