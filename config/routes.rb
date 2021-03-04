98c6f8cf4d29d89f95d361f8e4eeb175efbc40cd
sevil hatipoglu <sevilhatipogluu93@gmail.com>
2021-03-04 10:19:04 +0000

    Added: Accept and Decline button in dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :artworks, only: [:index, :show, :new, :create, :edit, :destroy] do 
+  resources :artworks, only: [:index, :show, :new, :create, :edit, :destroy] do
   resources :bookings, only: [:create]
   end
   resource :dashboard, only: :show
+  resources :bookings, only: [:update]
+
 end
