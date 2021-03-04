5d0382c078c2f31eab57d80c01b55a54615416e1
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-03-04 19:38:20 +0300

    amended error messages

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -10,16 +10,16 @@ class Booking < ApplicationRecord
 
   def end_date_cannot_come_before_start_date
     if end_date.present? && end_date < start_date
-      errors.add(:end_date, "end date must come after start date")
+      errors.add(:end_date, "must come after start date")
     end
   end
 
   def booking_dates_cannot_be_in_the_past
     if start_date.present? && start_date < Date.today
-      errors.add(:start_date, "You cannot make a booking in the past")
+      errors.add(:start_date, "cannot be in the past")
     end
     if end_date.present? && end_date < Date.today
-      errors.add(:end_date, "You cannot make a booking in the past")
+      errors.add(:end_date, "cannot be in the past")
     end
   end
 end
