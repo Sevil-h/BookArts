4d353677fc684e4e023833c9de8334ad2991a526
sevil hatipoglu <sevilhatipogluu93@gmail.com>
2021-03-29 23:00:59 +0300

    Changed map style

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,17 +3,17 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-  
+
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   };
-  
+
   const addMarkersToMap = (map, markers) => {
     markers.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
-      new mapboxgl.Marker()
+      new mapboxgl.Marker({color: '#443446'})
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup) // add this
       .addTo(map);
@@ -23,7 +23,7 @@ const initMapbox = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/liliumm/ckmuzlhhc01si17mncrjtx5zb'
   });
   const markers = JSON.parse(mapElement.dataset.markers);
   markers.forEach((marker) => {
