2f72c3b7aeacdf87cc70a483c6d7f13e9fb194e4
Hendrik Vermeersch <hendrik.vermeersch@protonmail.com>
2021-04-02 15:41:49 +0300

    fixed font size and color of spans in booking cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import swal from 'sweetalert';
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import { initMapbox } from '../plugins/init_mapbox';
 import { initSweetalert } from '../plugins/init_sweetalert';
 
 Rails.start()
@@ -25,23 +25,19 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-initSweetalert('#sweet-alert-demo', {
-  title: "Are you sure?",
-  text: "This action cannot be reversed",
-  icon: "warning"
-}, (value) => {
-  if (value) {
-    const link = document.querySelector('#delete-link');
-    link.click();
-  }
-});
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
-});
-import { initMapbox } from '../plugins/init_mapbox';
-
-document.addEventListener('turbolinks:load', () => {
   initMapbox();
-})
+  initSweetalert('sweet-alert', {
+    title: "Are you sure?",
+    text: "This action cannot be reversed",
+    icon: "warning"
+    // }
+    // , (value) => {
+    //   if (value) {
+    //     const link = document.querySelector('#delete-link');
+    //     link.click();
+    //   }
+  });
+});
